fc81a48b26ab7a1e462e6d4d0411eab4f0195e28
Adrián Moreno <90756596+Descalabreitor@users.noreply.github.com>
2024-04-04 10:03:51 +0100

    Resizes

---
+++ utils/MetadataOperator.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+class MetadataOperator:
+    def __init__(self, data_directory):
+        self.metadata = self.__read_metadata(data_directory)
+
+    @staticmethod
+    def __read_metadata(directory):
+        metadata = pd.read_csv(directory + "/metadata.csv", sep=",")
+        metadata = metadata[metadata["n"] < 9]
+        return metadata
+
+    def get_metadata(self):
+        return self.metadata
+
+    def get_best_revisits_id(self, data_point, n_revisits):
+        data_point_metadata = self.metadata[self.metadata['ID'] == data_point]
+        sorted_revisits = data_point_metadata.sort_values(by=['cloud_cover'])
+        return list(sorted_revisits['n'])[:n_revisits]
+
+    def sampleData(self, n):
+        return self.metadata['ID'].sample(n=n)
